c1fab99691ed5aa0172e33e58e77cce43ab2ec91
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2017-07-14 11:01:42 -0700

    mean differences
    added a new dialog for mean diff

---
+++ MetaModel/R/metamdms.b.R
@@ -0,0 +1,57 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+metamdmsClass <- R6::R6Class(
+    "metamdmsClass",
+    inherit = metamdmsBase,
+    private = list(
+      .run = function() {
+        n1i <- self$options$N1
+        m1i <- self$options$M1
+        sd1i <- self$options$SD1
+        n2i <- self$options$N2
+        m2i <- self$options$M2
+        sd2i <- self$options$SD2
+        #mods <- self$options$moderatorcor
+        #slab <- self$options$studylabels
+        method2 <- self$options$methodmetamdms
+        mdmseasure <- self$options$mdmsmeasure
+        
+        #data <- self$data
+        
+        data <- data.frame(n1i = self$data[[self$options$N1]], 
+                           m1i = self$data[[self$options$M1]],
+                           sd1i = self$data[[self$options$SD1]],
+                           n2i = self$data[[self$options$N2]], 
+                           m2i = self$data[[self$options$M2]],
+                           sd2i = self$data[[self$options$SD2]]
+                           )
+        
+        data[[n1i]] <- jmvcore::toNumeric(data[[n1i]])
+        data[[m1i]] <- jmvcore::toNumeric(data[[m1i]])
+        data[[sd1i]] <- jmvcore::toNumeric(data[[sd1i]])
+        data[[n2i]] <- jmvcore::toNumeric(data[[n2i]])
+        data[[m2i]] <- jmvcore::toNumeric(data[[m2i]])
+        data[[sd2i]] <- jmvcore::toNumeric(data[[sd2i]])
+        
+        
+        res <- metafor::rma(n1i=n1i, n2i=n2i, m1i=m1i, m2i=m2i, sd1i=sd1i, sd2i=sd2i,
+                            method=method2, measure=mdmseasure, data=data)
+        
+        self$results$text$setContent(res)
+        # `self$data` contains the data
+        # `self$options` contains the options
+        # `self$results` contains the results object (to populate)
+        image <- self$results$plot
+        image$setState(res)        
+      },
+      .plot=function(image, ...) {  # <-- the plot function
+        plotData <- image$state
+        #yi <- self$options$yi
+        #vi <- self$options$vi
+        #res <- metafor::rma(yi=yi, vi=vi, data=self$data) 
+        plot <- metafor::forest(plotData)
+        print(plot)
+        TRUE
+      })
+)
---
+++ MetaModel/build/js/metamdms.src.js
@@ -0,0 +1,126 @@
+
+// This file is an automatically generated and should not be edited
+
+'use strict';
+
+const options = [{"name":"data","type":"Data"},{"name":"N1","title":"Group One Sample Size","type":"Variable"},{"name":"M1","title":"Group One Mean","type":"Variable"},{"name":"SD1","title":"Group One Stand Deviation","type":"Variable"},{"name":"N2","title":"Group Two Sample Size","type":"Variable"},{"name":"M2","title":"Group Two Mean","type":"Variable"},{"name":"SD2","title":"Group Two Stand Deviation","type":"Variable"},{"name":"methodmetamdms","title":"Model Estimator","type":"List","options":[{"name":"DL","title":"DerSimonian-Laird"},{"name":"HE","title":"Hedges"},{"name":"HS","title":"Hunter-Schmidt"},{"name":"SJ","title":"Sidik-Jonkman"},{"name":"ML","title":"Maximum-Likelihood"},{"name":"REML","title":"Restricted Maximum-Likelihood"},{"name":"EB","title":"Empirical Bayes"},{"name":"PM","title":"Paule-Mandel"},{"name":"FE","title":"Fixed-Effect"}],"default":"REML"},{"name":"mdmsmeasure","title":"Model Measures","type":"List","options":[{"name":"MD","title":"Raw Mean Difference"},{"name":"SMD","title":"Standardized Mean Difference"},{"name":"SMDH","title":"SMD with Heteroscedastic Population Variances"},{"name":"ROM","title":"Log Transformed Ratio of Means"}],"default":"SMD"}];
+
+const view = View.extend({
+    jus: "2.0",
+
+    events: [
+
+	]
+
+});
+
+view.layout = ui.extend({
+
+    label: "Meta-Analysis - Mean Differences",
+    jus: "2.0",
+    type: "root",
+    stage: 0, //0 - release, 1 - development, 2 - proposed
+    controls: [
+		{
+			type: DefaultControls.VariableSupplier,
+			persistentItems: false,
+			stretchFactor: 1,
+			controls: [
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group One Sample Size",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "N1",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group One Mean",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "M1",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group One Stand Deviation",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "SD1",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group Two Sample Size",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "N2",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group Two Mean",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "M2",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					label: "Group Two Stand Deviation",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							name: "SD2",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.ComboBox,
+					name: "methodmetamdms"
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.ComboBox,
+					name: "mdmsmeasure"
+				}
+			]
+		}
+	]
+});
+
+module.exports = { view : view, options: options };
